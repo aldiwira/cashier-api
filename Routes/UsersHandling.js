fbd8c8d5969eef3b9be76abaf0c02dd50d00a202
aldi wirawan <aldiwira.aw@gmail.com>
2020-10-30 14:24:19 +0700

    fixing user management

--- Routes/UsersHandling.js
+++ Routes/UsersHandling.js
@@ -1,41 +1,36 @@
-const router = require("express").Router();
-const { doFormat, dateNow } = require("../Helper/response");
-const { getCollection } = require("../Db");
-const { registerModel, loginModel } = require("../Helper/validator");
-const { doSignToken } = require("../Helper/jwt");
-const { decrypt, encrypt } = require("../Helper/encrypt");
+const router = require('express').Router();
+const { doFormat, dateNow } = require('../Helper/response');
+const { getCollection } = require('../Db');
+const { registerModel, loginModel } = require('../Helper/validator');
+const { doSignToken } = require('../Helper/jwt');
+const { decrypt, encrypt } = require('../Helper/encrypt');
 
-let usersModels = getCollection("users");
+let usersModels = getCollection('users');
 
-router.post("/register", async (req, res, next) => {
-  let { username, password, store_name, owner_name } = req.body;
+router.post('/register', async (req, res, next) => {
+  let { username, password, store_name } = req.body;
   try {
     await registerModel().validate(req.body);
     const exsist = await usersModels.findOne({
-      $or: [
-        { username: username },
-        { store_name: store_name },
-        { owner_name: owner_name },
-      ],
+      $or: [{ username: username }, { store_name: store_name }]
     });
     if (exsist) {
-      throw new Error("Your registered account was availabe");
+      throw new Error('Your registered account was availabe');
     } else {
       const body = {
         username,
         password: await encrypt(password),
         store_name,
-        owner_name,
         lastLogin: dateNow(),
         createdAt: dateNow(),
-        updatedAt: dateNow(),
+        updatedAt: dateNow()
       };
       const userReg = await usersModels.insert(body);
       const userToken = await doSignToken(userReg._id);
       res.status(201).json(
-        doFormat(201, "Success register account", {
+        doFormat(201, 'Success register account', {
           user: userReg,
-          token: userToken,
+          token: userToken
         })
       );
     }
@@ -44,20 +39,20 @@ router.post("/register", async (req, res, next) => {
   }
 });
 
-router.post("/login", async (req, res, next) => {
+router.post('/login', async (req, res, next) => {
   const { username, password } = req.body;
   try {
     await loginModel().validate(req.body);
     await usersModels
       .findOneAndUpdate(
         {
-          username: username,
+          username: username
         },
         {
           $set: {
             lastLogin: dateNow(),
-            updatedAt: dateNow(),
-          },
+            updatedAt: dateNow()
+          }
         }
       )
       .then(async (result) => {
@@ -65,16 +60,16 @@ router.post("/login", async (req, res, next) => {
           if (await decrypt(password, result.password)) {
             const userToken = await doSignToken(result._id);
             res.status(200).json(
-              doFormat(200, "Success login account", {
+              doFormat(200, 'Success login account', {
                 user: result,
-                token: userToken,
+                token: userToken
               })
             );
           } else {
-            throw new Error("Wrong password account");
+            throw new Error('Wrong password account');
           }
         } else {
-          throw new Error("Account not found");
+          throw new Error('Account not found');
         }
       });
   } catch (error) {
