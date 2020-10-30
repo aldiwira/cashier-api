0c36b1c1a33c45c13de8f292580ccadcf27937fc
aldi wirawan <aldiwira.aw@gmail.com>
2020-10-30 14:25:27 +0700

    fixing db connection

--- Db/index.js
+++ Db/index.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV;
 const cdb = require('./config.db.json')[env];
 const monk = require('monk');
 
-const db = monk(cdb.url + cdb.database);
+const db = monk(cdb.url + '/' + cdb.database);
 
 const getCollection = (collection) => {
   return db.get(collection);
