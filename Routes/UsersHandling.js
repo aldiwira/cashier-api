846bc87a0439a92247a270b1d87e37ccd0d233aa
aldi wirawan <aldiwira.aw@gmail.com>
2020-11-24 21:05:20 +0700

    add user profil data

--- Routes/UsersHandling.js
+++ Routes/UsersHandling.js
@@ -2,10 +2,35 @@ const router = require('express').Router();
 const { doFormat, dateNow } = require('../Helper/response');
 const { getCollection } = require('../Db');
 const { registerModel, loginModel } = require('../Helper/validator');
-const { doSignToken } = require('../Helper/jwt');
+const { doSignToken, doAuthToken } = require('../Helper/jwt');
 const { decrypt, encrypt } = require('../Helper/encrypt');
 
 let usersModels = getCollection('users');
+
+router.get('/user/:idUser/profile', doAuthToken, async (req, res, next) => {
+  let { idUser } = req.params;
+  try {
+    await usersModels.findOne(idUser).then((data) => {
+      const profileList = data.profile;
+      res
+        .status(200)
+        .json(doFormat(200, 'Success fetch profile data', profileList));
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/user/:idUser', doAuthToken, async (req, res, next) => {
+  let { idUser } = req.params;
+  try {
+    await usersModels.findOne(idUser).then((data) => {
+      res.status(200).json(doFormat(200, 'Success fetch profile data', data));
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post('/register', async (req, res, next) => {
   let { username, password, store_name } = req.body;
@@ -17,10 +42,12 @@ router.post('/register', async (req, res, next) => {
     if (exsist) {
       throw new Error('Your registered account was availabe');
     } else {
+      let profile = ['Kasir', 'Dapur', 'Pramusaji'];
       const body = {
         username,
         password: await encrypt(password),
         store_name,
+        profile,
         lastLogin: dateNow(),
         createdAt: dateNow(),
         updatedAt: dateNow()
